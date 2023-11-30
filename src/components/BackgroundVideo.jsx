47c05debb2bed3415e790c04f46df8d8fcb48583
Samuel Adebayo <repzysam@gmail.com>
2023-11-30 17:35:38 +0100

    some changes

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -3,9 +3,11 @@ import React from "react";
 export const BackgroundVideo = () => {
   return (
     <div className="relative">
-      <video className="h-[400px] rounded-xl shadow-lg" autoPlay muted loop>
-        <source src="src\assets\Marvel Opening Theme.mp4" type="video/mp4" />
-      </video>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/UhVjp48U2Oc?si=v6q3jsQi0E224xud&amp;controls=0"
+      ></iframe>
     </div>
   );
 };
